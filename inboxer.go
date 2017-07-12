5c51db057ba599a660108d1737bc78129c58322b
Johnathan Hartsfield <johnathanhartsfield@gmail.com>
2017-07-12 18:11:24 +0000

    updates

--- inboxer.go
+++ inboxer.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"gitlab.com/hartsfield/gmailAPI"
@@ -12,10 +13,6 @@ import (
 )
 
 func main() {
-	check()
-}
-
-func check() {
 	// Connect to the gmail API service.
 	ctx := context.Background()
 	srv := gmailAPI.ConnectToService(ctx, gmail.MailGoogleComScope)
@@ -26,24 +23,65 @@ func check() {
 		fmt.Println(err)
 	}
 
+	fmt.Println("--------------------------------------------------------")
 	// Range over the messages
-	for _, v := range msgs.Messages[:5] {
+	for _, v := range msgs.Messages[:10] {
 		msg, _ := srv.Users.Messages.Get("me", v.Id).Do()
-
-		fmt.Println(msg.Snippet)
-		fmt.Println(msg.LabelIds)
-
-		// Time
-		fmt.Println(getTime(msg.InternalDate))
-
-		// dec, err := decodeEmailBody(msg.Payload.Parts[0].Body.Data)
-		// if err != nil {
-		// 	fmt.Println(err)
-		// }
-		// fmt.Println(dec)
-
+		info := getSender2(msg)
+		fmt.Println(info.From, info.Sender, info.Subject)
+		// fmt.Println(getSender(msg), getTime(msg.InternalDate))
+		if getByLabel(strings.ToUpper("UNREAD"), msg) {
+			// fmt.Println(msg.Snippet)
+			// fmt.Println("--------------------------------------------------------")
+		}
 	}
+}
 
+func getBody(msg *gmail.Message) (string, error) {
+	dec, err := decodeEmailBody(msg.Payload.Parts[0].Body.Data)
+	if err != nil {
+		return "", err
+	}
+	return dec, nil
+}
+
+func getByLabel(label string, msg *gmail.Message) bool {
+	for _, v := range msg.LabelIds {
+		if v == label {
+			return true
+		}
+	}
+	return false
+}
+
+type senderInfo struct {
+	Sender, From, To, CC, Subject, MailingList, DeliveredTo, ThreadTopic []string
+}
+
+func getSender2(msg *gmail.Message) *senderInfo {
+	info := &senderInfo{}
+	fmt.Println("--------------------------------------------------------")
+	for _, v := range msg.Payload.Headers {
+		switch v.Name {
+		case "Sender":
+			info.Sender = append(info.Sender, v.Value)
+		case "From":
+			info.From = append(info.From, v.Value)
+		case "To":
+			info.To = append(info.To, v.Value)
+		case "CC":
+			info.CC = append(info.CC, v.Value)
+		case "Subject":
+			info.Subject = append(info.Subject, v.Value)
+		case "Mailing-list":
+			info.MailingList = append(info.MailingList, v.Value)
+		case "Delivered-To":
+			info.DeliveredTo = append(info.DeliveredTo, v.Value)
+		case "Thread-Topic":
+			info.ThreadTopic = append(info.ThreadTopic, v.Value)
+		}
+	}
+	return info
 }
 
 func decodeEmailBody(data string) (string, error) {
@@ -57,10 +95,35 @@ func decodeEmailBody(data string) (string, error) {
 
 func getTime(datetime int64) time.Time {
 	conv := strconv.FormatInt(datetime, 10)
-	cut := s[:len(s)-3]
-	tc, err := strconv.ParseInt(t, 10, 64)
+	cut := conv[:len(conv)-3]
+	tc, err := strconv.ParseInt(cut, 10, 64)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return time.Unix(tc, 0)
 }
+
+// func watchInbox() {
+// 	req := &gmail.WatchRequest{
+// 		LabelFilterAction: "include",
+// 		LabelIds:          []string{"UNREAD"},
+// 		TopicName:         "gmailmsg",
+// 	}
+
+// 	wr, _ := srv.Users.Watch("me", req).Do()
+// 	fmt.Println(wr.ForceSendFields)
+// }
+
+// func getMessages() (*gmail.ListMessagesResponse, error) {
+// 	// Connect to the gmail API service.
+// 	ctx := context.Background()
+// 	srv := gmailAPI.ConnectToService(ctx, gmail.MailGoogleComScope)
+
+// 	// Get the messages
+// 	msgs, err := srv.Users.Messages.List("me").Do()
+// 	if err != nil {
+// 		return &gmail.ListMessagesResponse{}, err
+// 	}
+
+// 	return msgs, nil
+// }
